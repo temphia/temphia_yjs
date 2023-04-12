c6bb432e943949412c1d51cbf80981f69f5c9af3
bornjre <bornjre@gmail.com>
2023-04-12 06:48:44 +0545

    lib generate

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,3 +1,13 @@
-{
-    
+const path = require('path')
+
+module.exports = {
+    entry: {
+        easy_provider: './src/easy_provider.js'
+    },
+    output: {
+        path: path.resolve('./dist'),
+        filename: '[name].js',
+        library: 'temphia_yjs',
+        libraryTarget: 'umd'
+    },
 }
